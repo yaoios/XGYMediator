c34a45d784874a720bf29fcdd4938d8ec32d5202
耀哥 <yao_ios@163.com>
2019-06-03 17:37:55 +0800

    first commit

---
+++ XGYMediator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XGYMediator
+//
+//  Created by xuguangyao on 2019/6/3.
+//  Copyright © 2019年 com.haqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
